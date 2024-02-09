1b68f0a061238c97ee0545b19ab0548ef9aedc7d
Alexander Lyon <arlyon@me.com>
2024-02-09 18:50:44 +0000

    make LSP use blocking version of package discovery
    There are a few cases where we want to be able to fall back.

--- crates/turborepo-filewatch/src/package_watcher.rs
+++ crates/turborepo-filewatch/src/package_watcher.rs
@@ -61,6 +61,27 @@ impl PackageDiscovery for WatchingPackageDiscovery {
             package_manager,
         })
     }
+
+    // if the event that either of the dependencies will never resolve,
+    // this will still return unavailable
+    async fn discover_packages_blocking(&self) -> Result<DiscoveryResponse, discovery::Error> {
+        let package_manager = self
+            .watcher
+            .wait_for_package_manager()
+            .await
+            .map_err(|_| discovery::Error::Unavailable)?;
+
+        let workspaces = self
+            .watcher
+            .wait_for_package_data()
+            .await
+            .map_err(|_| discovery::Error::Unavailable)?;
+
+        Ok(DiscoveryResponse {
+            workspaces,
+            package_manager,
+        })
+    }
 }
 
 /// Watches the filesystem for changes to packages and package managers.
